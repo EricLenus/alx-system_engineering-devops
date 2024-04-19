2b13c7b6f0bdbd0a3e1a649006657947ed4c497a
EricLenus <ericledama@gmail.com>
2024-04-19 15:50:24 -0700

    task 01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # install puppet-lint -v 2.1.0
 
 exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 3.1.6',
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
 
 
