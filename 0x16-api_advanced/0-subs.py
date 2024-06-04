e957a0572c11a99d2fbfb44f91d9b6d3a6983e75
EricLenus <ledamaeric@gmail.com>
2024-06-04 14:53:40 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,21 @@
+#!/usr/bin/python3
+'''Returns the number of subscribers of a given subreddit'''
 import requests
 
-def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'python:subreddit.subscriber.count:v1.0 (by /u/yourusername)'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
 
-# Example usage:
-# print(number_of_subscribers("learnpython"))
+def number_of_subscribers(subreddit):
+    '''Retrieves number of subscribers of a subreddit community'''
+    if not subreddit or not isinstance(subreddit, str):
+        return 0
+    api_url = "https://www.reddit.com"
+    endpoint = f"/r/{subreddit}/about.json"
+    headers = {'User-agent': 'Mozilla/5.0'}
+
+    url = api_url + endpoint
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+
+    if resp.status_code != 200:
+        return 0
+    data = resp.json()
+    return data.get('data').get('subscribers')
+
