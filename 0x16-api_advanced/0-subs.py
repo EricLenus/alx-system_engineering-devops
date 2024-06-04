102cae7e1a1842e865fcd3308dd00ba6c9248238
EricLenus <ledamaeric@gmail.com>
2024-06-04 14:24:00 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-'''Returns the number of subscribers of a given subreddit'''
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    '''Retrieves number of subscribers of a subreddit community'''
-    if not subreddit or not isinstance(subreddit, str):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    api_url = "https://www.reddit.com"
-    endpoint = f"/r/{subreddit}/about.json"
-    headers = {'User-agent': 'Mozilla/5.0'}
-
-    url = api_url + endpoint
-    resp = requests.get(url, headers=headers, allow_redirects=False)
-
-    if resp.status_code != 200:
-        return 0
-    data = resp.json()
-    return data.get('data').get('subscribers')
