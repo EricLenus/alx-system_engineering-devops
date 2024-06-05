d6d05ad064775f5087331e9712ecc71fb429dbbb
EricLenus <ledamaeric@gmail.com>
2024-06-05 14:55:38 -0700

    strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Debug wordpress running on apache with php
+
+exec { 'typo_fix':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
